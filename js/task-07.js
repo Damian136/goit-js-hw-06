59bb8c03353dbf430bbff084d728a0f6730e50ec
Damian Nowak <108193148+Damian136@users.noreply.github.com>
2023-05-08 15:32:12 +0200

    zadanie 7

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+'use strict';
+const fontSizeControl = document.getElementById('font-size-control');
+const text = document.getElementById('text');
+fontSizeControl.addEventListener('input', () => {
+    text.style.fontSize = `${fontSizeControl.value}px`;
+});
